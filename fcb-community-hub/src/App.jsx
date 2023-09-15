cbfbcc431c7e5f2e30f6a952e80ab4c8bb812219
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-15 19:12:58 -0400

    Add background image

--- fcb-community-hub/src/App.jsx
+++ fcb-community-hub/src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
-import Header from './components/Header'
-import Event from './components/Event'
-import EventContainer from './components/EventContainer'
+import './App.css';
+import Header from './components/Header';
+import EventContainer from './components/EventContainer';
+import BackgroundImage from './assets/fcb-huddle.png'; // Import the background image
 
 function App() {
+  const backgroundStyle = {
+    background: `rgba(0, 0, 0, 0.1) url(${BackgroundImage})`, // Adjust the opacity value (0.1 for 10% opacity)
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh', // Set a minimum height to fill the screen
+  };
+
   return (
-    <>
+    <div className='mb-16' style={backgroundStyle}>
       <Header />
       <EventContainer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
