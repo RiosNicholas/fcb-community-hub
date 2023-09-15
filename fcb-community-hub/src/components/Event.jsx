6d2ad4f7ec2df2e450d18a271163087aef9d5f45
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-15 17:11:43 -0400

    Update event grid and update css

--- fcb-community-hub/src/components/Event.jsx
+++ fcb-community-hub/src/components/Event.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const Event = (props) => {
     return (
-        <div>
-            <img></img>
-            <h2 className="text-black">FCB at {props.opponent}</h2>
-            <p>Match details</p>
-            <button>Add to Calendar</button>
+        <div className="m-5 p-8 bg-red-100 w-4/5 h-auto rounded-lg shadow-white shadow-sm text-center flex flex-col justify-center items-center">
+            <img src="your-image-source" alt="Event Image" className="w-32 h-32 object-cover rounded-full" />
+            <div>
+                <h2 className="text-blue-950 font-extrabold text-xl">
+                    {props.homeTeam} vs {props.awayTeam}
+                </h2>
+            </div>
+            <p className="italic text-black font-bold">{props.date}</p>
+            <a href={props.matchInfo}>
+                <button className="bg-red-700 hover:bg-red-800 w-48 mt-2 p-1 rounded font-bold text-center">Match Info</button>
+            </a>
         </div>
     )
 }
--- fcb-community-hub/src/components/EventContainer.jsx
+++ fcb-community-hub/src/components/EventContainer.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Event from "./Event";
+// import '../assets/';
 
 const EventContainer = () => {
     return (
-        <div>
-            <Event />
-            <Event />
-            <Event />
-            <Event />
+        <div className="grid m-10 sm:grid-cols-1 md:grid-cols-2">
+            <Event homeTeam='FC Barcelona' awayTeam='Real Betis' date='September 15, 2023' matchInfo="https://onefootball.com/en/match/2397169"/>
+            <Event homeTeam='Barcelona' awayTeam='Celta Vigo' date='September 23, 2023' matchInfo="https://onefootball.com/en/match/2397181" />
+            <Event homeTeam='Mallorca' awayTeam='FC Barcelona' date='September 26, 2023' matchInfo="https://onefootball.com/en/match/2397192" />
+            <Event homeTeam='FC Barcelona' awayTeam='Sevilla' date='September 29, 2023' matchInfo="https://onefootball.com/en/match/2397201" />
+            <Event homeTeam='Granada' awayTeam='FC Barcelona' date='October 8, 2023' matchInfo="https://onefootball.com/en/match/2397213" />
+            <Event homeTeam='FC Barcelona' awayTeam='Athletic Club' date='October 22, 2023' matchInfo="https://onefootball.com/en/match/2397218" />
+            <Event homeTeam='FC Barcelona' awayTeam='Real Madrid' date='October 28, 2023' matchInfo="https://onefootball.com/en/match/2397231" />
+            <Event homeTeam='Real Sociedad' awayTeam='FC Barcelona' date='November 5, 2023' matchInfo="https://onefootball.com/en/match/2397245" />
+            <Event homeTeam='FC Barcelona' awayTeam='Alavéz' date='November 12, 2023' matchInfo="https://onefootball.com/en/match/2397251" />
+            <Event homeTeam='Rayo Vallecano' awayTeam='FC Barcelona' date='November 26, 2023' matchInfo="https://onefootball.com/en/match/2397263" />
         </div>
     )
 }
