3617ac1ce97728af50a21eb50223e78ab5eff85c
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-15 20:35:34 -0400

    Remove redundant background color

--- fcb-community-hub/src/App.jsx
+++ fcb-community-hub/src/App.jsx
@@ -5,11 +5,11 @@ import BackgroundImage from './assets/fcb-huddle.png'; // Import the background 
 
 function App() {
   const backgroundStyle = {
-    background: `rgba(0, 0, 0, 0.1) url(${BackgroundImage})`, // Adjust the opacity value (0.1 for 10% opacity)
+    background: `url(${BackgroundImage})`, 
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
-    minHeight: '100vh', // Set a minimum height to fill the screen
+    minHeight: '100vh', 
   };
 
   return (
